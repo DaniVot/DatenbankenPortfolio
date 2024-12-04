91a7d798be36ba9e6f7df70933c8c9d1c663ce5b
DaniVot <144905638+DaniVot@users.noreply.github.com>
2024-12-04 14:17:29 +0100

    Update masterScript.py

--- scripts/masterScript.py
+++ scripts/masterScript.py
@@ -1,18 +1,18 @@
 import subprocess
 import os
 
-# Get the base directory (assuming this script is in the folder "3. data cleansing")
-base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# Get the base directory (assuming this script is in "scripts")
+base_dir = os.path.dirname(__file__)
 
-# Define the scripts to execute in order, with their relative paths
+# Define the scripts to execute in the desired sequence
 scripts = [
     os.path.join(base_dir, "2. faking data", "fakingScript.py"),
+    os.path.join(base_dir, "3. data cleansing", "deleteRestaurantsWithoutInspections.py"),
     os.path.join(base_dir, "3. data cleansing", "consistencyCheck.py"),
-    os.path.join(base_dir, "3. data cleansing", "consistencyEnforcer.py"),
-    os.path.join(base_dir, "3. data cleansing", "deleteEmptyInspection.py"),
-    os.path.join(base_dir, "3. data cleansing", "deleteEmptyYelpBusiness.py"),
+    os.path.join(base_dir, "3. data cleansing", "consistencyEnforcement.py"),
     os.path.join(base_dir, "3. data cleansing", "searchDuplicates.py"),
-    os.path.join(base_dir, "4. mySQLImport", "import.py")
+    os.path.join(base_dir, "3. data cleansing", "dataTypesCheck.py"),
+    os.path.join(base_dir, "4. mySQLImport", "import.py"),
 ]
 
 # Loop through and execute each script
