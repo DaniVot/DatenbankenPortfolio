4cd79762f1972702363cb6370f0865160d21a5b1
DaniVot <144905638+DaniVot@users.noreply.github.com>
2024-12-05 15:24:29 +0100

    Update coordinatesExport.py

--- scripts/4. mySQL/coordinatesExport.py
+++ scripts/4. mySQL/coordinatesExport.py
@@ -12,33 +12,41 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 # Drop views if they already exist
-cursor.execute("DROP VIEW IF EXISTS bad_inspections")
-cursor.execute("DROP VIEW IF EXISTS ok_inspections")
-cursor.execute("DROP VIEW IF EXISTS good_inspections")
+cursor.execute("DROP VIEW IF EXISTS A_inspections")
+cursor.execute("DROP VIEW IF EXISTS B_inspections")
+cursor.execute("DROP VIEW IF EXISTS C_inspections")
+cursor.execute("DROP VIEW IF EXISTS all_inspections")
 
 # Create views
 cursor.execute("""
-    CREATE VIEW bad_inspections AS
-    SELECT ir.business_id, ir.inspection_date, yb.latitude, yb.longitude
+    CREATE VIEW A_inspections AS
+    SELECT ir.business_id, ir.inspection_date, ir.score, yb.latitude, yb.longitude, yb.borough
     FROM inspection_results ir
     JOIN yelp_business yb ON ir.business_id = yb.business_id
-    WHERE ir.score BETWEEN 0 AND 33
+    WHERE ir.score BETWEEN 0 AND 13
 """)
 
 cursor.execute("""
-    CREATE VIEW ok_inspections AS
-    SELECT ir.business_id, ir.inspection_date, yb.latitude, yb.longitude
+    CREATE VIEW B_inspections AS
+    SELECT ir.business_id, ir.inspection_date, ir.score, yb.latitude, yb.longitude, yb.borough
     FROM inspection_results ir
     JOIN yelp_business yb ON ir.business_id = yb.business_id
-    WHERE ir.score BETWEEN 34 AND 66
+    WHERE ir.score BETWEEN 14 AND 27
 """)
 
 cursor.execute("""
-    CREATE VIEW good_inspections AS
-    SELECT ir.business_id, ir.inspection_date, yb.latitude, yb.longitude
+    CREATE VIEW C_inspections AS
+    SELECT ir.business_id, ir.inspection_date, ir.score, yb.latitude, yb.longitude, yb.borough
     FROM inspection_results ir
     JOIN yelp_business yb ON ir.business_id = yb.business_id
-    WHERE ir.score BETWEEN 67 AND 100
+    WHERE ir.score BETWEEN 28 AND 100
+""")
+
+cursor.execute("""
+    CREATE VIEW all_inspections AS
+    SELECT ir.business_id, ir.inspection_date, ir.score, yb.latitude, yb.longitude, yb.borough
+    FROM inspection_results ir
+    JOIN yelp_business yb ON ir.business_id = yb.business_id
 """)
 
 # Commit and close connection
